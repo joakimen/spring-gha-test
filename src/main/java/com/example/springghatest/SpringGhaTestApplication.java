488bb567b9fb5aa4fe68d0bcdcdcc2b78486cbb5
Joakim Engeset <joakim.engeset@gmail.com>
2021-02-20 16:03:39 +0000

    Add  baseline spring web project

---
+++ src/main/java/com/example/springghatest/SpringGhaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springghatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGhaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGhaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springghatest/SpringGhaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springghatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGhaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
